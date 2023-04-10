c0a08b3d21ba7f7e665f84b07b58404693e4db8b
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-04-10 23:15:45 +0600

    Feture Job

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Jobs = () => {
     return (
         <div>
-            Hello From JObs
+            <div className='text-center mt-32 mb-8'>
+                <h1 className='text-4xl font-bold mb-2'>Featured Jobs</h1>
+                <p className='mx-4'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
     );
 };
