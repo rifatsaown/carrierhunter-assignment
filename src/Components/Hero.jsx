7dc4becfc5c58f3115193b4bce1184d3c33923e8
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-04-14 01:22:42 +0600

    Done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,10 @@ const Hero = () => {
     <div>
       <div className="hero min-h-[90vh] bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={profile} className=" lg:max-w-2xl rounded-lg lg:mb-[-20px] " />
+          <img
+            src={profile}
+            className=" lg:max-w-2xl rounded-lg lg:mb-[-20px] "
+          />
           <div className="max-w-[570px]">
             <h1 className="text-6xl font-bold lg:max-w-[330px]">
               One Step Closer To Your
@@ -17,7 +20,9 @@ const Hero = () => {
               you need. Its your future. Come find it. Manage all your job
               application from start to finish.
             </p>
-            <a href="#jobs"  className="btn btn-secondary">Get Started</a>
+            <a href="#jobs" className="btn btn-secondary">
+              Get Started
+            </a>
           </div>
         </div>
       </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
 import Category from "./Category";
 import Hero from "./Hero";
 import Jobs from "./Jobs";
 
 const Home = () => {
-  
   return (
     <>
-      
-        <Hero></Hero>
-        <Category></Category>
-        <Jobs></Jobs>
+      <Hero></Hero>
+      <Category></Category>
+      <Jobs></Jobs>
     </>
   );
 };
